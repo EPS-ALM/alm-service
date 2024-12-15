8820a3a35355c4e9a864aa770ec40bedf10a394a
Lucas Pimentel <luk.2001@hotmail.com>
2024-12-15 17:26:38 -0300

    feat: adds alm-assets deploy url

--- src/utils/constants/UrlConstants.ts
+++ src/utils/constants/UrlConstants.ts
@@ -3,6 +3,6 @@ require('dotenv').config();
 export default class UrlConstants {
     private static isDev = process.env.NODE_ENV === 'development'
     private static ip = process.env.IP as string
-    public static readonly URL_BASE_ALM_ASSETS = this.isDev ? `http://${this.ip}:8000` : "url-deploy";
+    public static readonly URL_BASE_ALM_ASSETS = this.isDev ? `http://${this.ip}:8000` : "https://alm-assets-898cc81ff044.herokuapp.com";
     public static readonly PATH_MARKOVITZ = `${UrlConstants.URL_BASE_ALM_ASSETS}/markowitz`;
 }
